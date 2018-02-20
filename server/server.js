901a1671f69c72deb82f3903e72463c8138bbfa4
Manisha2409 <manishamca65@enunui.com>
2018-02-20 12:27:04 +0530

    Listing Resources

--- server/server.js
+++ server/server.js
@@ -20,36 +20,19 @@ app.post('/todos',(req,res)=>{
           res.status(400).send(e);
       });
 });
+
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+       res.send({todos});
+  },(e)=>{
+    res.status(400).send(e);
+  })
+});
+
+
 app.listen(3000,()=>{
     console.log('Started listening on 3000 port');
 });
 
 
-
-// var express = require('express');
-// var bodyParser = require('body-parser');
-
-// var {mongoose} = require('./db/mongoose');
-// var {Todo} = require('./models/todo');
-// var {User} = require('./models/Users');
-
-// var app = express();
-
-// app.use(bodyParser.json());
-
-// app.post('/todos', (req, res) => {
-//   var todo = new Todo({
-//     text: req.body.text
-//   });
-
-//   todo.save().then((doc) => {
-//     res.send(doc);
-//   }, (e) => {
-//     res.status(400).send(e);
-//   });
-// });
-
-
-// app.listen(3000, () => {
-//   console.log('Started on port 3000');
-// });
+module.exports = {app};
