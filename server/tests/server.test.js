1a7ead76a2e3bafddb0a6971cb41c18e387876dd
Manisha2409 <manishamca65@enunui.com>
2018-02-21 13:44:18 +0530

    Testing Id values

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,13 @@ const request = require('supertest');
 const {app}=require('./../server');
 const {Todo}= require('./../models/todo');
 
+const {ObjectID} = require('mongodb');
+
 const todos = [{
+  _id: new ObjectID(),
   text : 'First test todos'
 },{
+  _id: new ObjectID(),
   text : 'Second test todos'
 }]
 
@@ -72,7 +76,32 @@ describe('GET /todos',()=>{
  });
 });
 
+describe('GET/ todos/:id',()=>{
+  it('Should return todo Document',(done)=>{
+      request(app)
+         .get(`/todos/${todos[0]._id.toHexString()}`)
+         .expect(200)
+         .expect((res)=>{
+           expect(res.body.todo.text).toBe(todos[0].text)
+         })
+         .end(done);
+  });
 
+  it('Should return 404 if todo not found',(done)=>{
+    var hexId= new ObjectID().toHexString;  
+    request(app)
+        .get(`/todos/${hexId}`)
+        .expect(404)
+        .end(done);
+  });
+
+  it('Should return 404 for non-object ids',(done)=>{
+    request(app)
+      .get(`/todos/1232343`)
+        .expect(404)
+        .end(done);
+  })
+});
 
 
 
