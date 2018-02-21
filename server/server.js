d069093dfd6901951cfd7f15b89071e8a8bfd4ab
Manisha2409 <manishamca65@enunui.com>
2018-02-21 12:18:23 +0530

    Add GET/ todos/:id

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {Users} = require('./models/Users.js');
-  
+
+const {ObjectID} = require('mongodb');
 
 var app=express();
 
@@ -29,6 +30,22 @@ app.get('/todos',(req,res)=>{
   })
 });
 
+
+app.get('/todos/:id',(req,res)=>{
+  var id= req.params.id;
+   if(!ObjectID.isValid(id)){
+        return res.status(404).send('Invalid ID');
+   }
+   Todo.findById(id).then((todo)=>{
+     if(!todo){
+        return res.status(400).send('Can\'t find Todo');
+     }
+      res.send({todo});
+     
+   }).catch((e)=>{
+      return res.status(404).send();
+   });
+});
 
 app.listen(3000,()=>{
     console.log('Started listening on 3000 port');
