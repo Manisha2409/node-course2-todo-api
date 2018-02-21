669c3e5b228c900400ca99a3e3af4946f4f303e5
Manisha2409 <manishamca65@enunui.com>
2018-02-21 10:19:07 +0530

    Add tests for GET/ todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,16 @@ const request = require('supertest');
 const {app}=require('./../server');
 const {Todo}= require('./../models/todo');
 
+const todos = [{
+  text : 'First test todos'
+},{
+  text : 'Second test todos'
+}]
+
 beforeEach((done)=>{
-    Todo.remove({}).then(()=> done());
+    Todo.remove({}).then(()=> {
+      return Todo.insertMany(todos);
+    }).then(()=>done());
 });
 
 describe('Post/ todos',()=>{
@@ -14,7 +22,7 @@ it('Should create a new todo',(done)=>{
 
   request(app)
   .post('/todos')
-  .send(text)
+  .send({text})
   .expect(200)
   .expect((res)=>{
       expect(res.body.text).toBe(text);
@@ -24,7 +32,7 @@ it('Should create a new todo',(done)=>{
           return done(err)  ;
         }
            
-    Todo.find().then((todo)=>{
+    Todo.find({text}).then((todo)=>{
         expect(todo.length).toBe(1);
         expect(todo[0].text).toBe(text);
         done();
@@ -44,11 +52,24 @@ it('Should not create new todo with invalid data',(done)=>{
             return done(err)  ;
           }
             Todo.find().then((todo)=>{
-          expect(todo.length).toBe(0);
-             done();
+          expect(todo.length).toBe(2);
+             done();  
       }).catch((e) => done(e));
     });
   });  
+});
+
+
+describe('GET /todos',()=>{
+ it('Should get all todos',(done)=>{
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todos.length).toBe(2);
+    })
+    .end(done);
+ });
 });
 
 
@@ -96,7 +117,7 @@ it('Should not create new todo with invalid data',(done)=>{
 //   //   request(app)
 //   //     .post('/todos')
 //   //     .send({})
-//   //     .expect(400)
+//   //     .expect(200)
 //   //     .end((err, res) => {  
 //   //       if (err) {
 //   //         return done(err);
